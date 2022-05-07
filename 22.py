01dd53ff6cbcce8763a3969c45350b67f2420660
V3il3DOraCl3 <javadaaghajanloo@gmail.com>
2022-05-07 11:05:28 +0430

    initial commit

---
+++ 22.py
@@ -0,0 +1,27 @@
+"""
+22. In a square 2D list the number of rows equals the nnumber of columns. Write a function that accepts
+a square 2D list and returns True if the left to right contents of any row equals the top to bottom
+contents of any column. If no row matches any column, the function returns False.
+"""
+
+# def checker(lst):
+
+
+# if __name__ == '__main__':
+lst2D = [[], []]
+size = int(input("enter size of matrix : "))
+ii = 0
+ltstest = [[0, 1], [3, 4]]
+# print(ltstest[0][1])
+condition = size ** 2
+print(size)
+for i in range(0, size):
+    print(i)
+    # lst2D.insert([0][1], i)
+
+# while i < size:
+#     # lst2D[i][i] = i
+#      i += 1
+#     print(i)
+print(lst2D)
+""" i cant :)))) """
---
+++ 23.py
@@ -0,0 +1,7 @@
+"""
+23. We can represent a Tic-Tac-Toe board as a 3  3 grid in which each position can hold one of the
+following three strings: "X", "O", or " ". Write a function named check_winner that accepts a
+3  3 list as a parameter. If "X" appears in a winning Tic-Tac-Toe pattern, the function should return
+the string "X". If "O" appears in a winning Tic-Tac-Toe pattern, the function should return the string
+"O". If no winning pattern exists, the function should return the string " ".
+"""
